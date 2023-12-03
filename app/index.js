f252e6ffd01165bcbe0e6de33dc8e8708d14d04d
kojirenan <kj.renan@icloud.com>
2023-12-03 14:19:06 -0300

    responsividade e correção de erros

--- app/index.js
+++ app/index.js
@@ -1,17 +1,17 @@
-import verifyNumber from "./verify.js";
+import verifyNumber from './verify.js';
 
-const randomNumber = () => Math.floor(Math.random() * 1000);
+const randomNumber = () => Math.floor(Math.random() * 100);
 const number = randomNumber();
 let tryNumber = 0;
 
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    tryNumber ++;
-    const inputNumber = parseInt(input.value);
+button.addEventListener('click', e => {
+  e.preventDefault();
+  tryNumber++;
+  const inputNumber = parseInt(input.value);
 
-    verifyNumber(number, inputNumber, tryNumber)
-    input.value = '';
-})
+  verifyNumber(number, inputNumber, tryNumber);
+  input.value = '';
+});
